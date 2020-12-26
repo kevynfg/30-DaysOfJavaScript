56257f3a0bfd85460af9855af66e0098c57dde0a
kevyn <kevynredlotus@gmail.com>
2020-12-25 22:29:29 -0300

    - day 8 complete

---
+++ day-8.js
@@ -0,0 +1,10 @@
+// Complete the factorial function below.
+function factorial(n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(4));
